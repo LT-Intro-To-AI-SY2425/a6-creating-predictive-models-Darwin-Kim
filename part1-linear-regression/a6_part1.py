c7411119eebd57306f51fa8cf8f7553f7017fa25
darwin <dkim10@cps.edu>
2024-12-06 09:55:51 -0600

    completed a6_part1.py

--- part1-linear-regression/a6_part1.py
+++ part1-linear-regression/a6_part1.py
@@ -4,21 +4,35 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 data = pd.read_csv("part1-linear-regression/blood_pressure_data.csv")
-x = data["Age"].values
-y = data["Blood Pressure"].values
+xAxis=data["Age"]
+yAxis=data['Blood Pressure']
+x = xAxis.values
+y = yAxis.values
+#print(data)
 
 # Use reshape to turn the x values into 2D arrays:
 x = x.reshape(-1,1)
 
 # Create the model
-
-# Find the coefficient, bias, and r squared values. 
-# Each should be a float and rounded to two decimal places. 
-
-
+model = LinearRegression().fit(x, y)
+# Find the coefficient, bias, and r squared values.
+# Each should be a float and rounded to two decimal places.
+coef = round(float(model.coef_[0]),2)
+intcpt = round(float(model.intercept_),2)
+r_squared = model.score(x, y)
+equation = coef*x + intcpt
 # Print out the linear equation and r squared value
-
+print(f"Equation: y = {coef}x + {intcpt} | r^2: {r_squared}")
 # Predict the the blood pressure of someone who is 43 years old.
 # Print out the prediction
+x_predict=42
+prediction = model.predict([[x_predict]])
+print(prediction)
 
 # Create the model in matplotlib and include the line of best fit
+plt.xlabel("Age")
+plt.ylabel("Systolic Blood Pressure")
+plt.title("Systolic Blood Pressure by Age")
+plt.scatter(x, y)
+plt.plot(x, equation, c="r", label="Line of Best Fit")
+plt.show()
--- part1-linear-regression/part1_writeup.md
+++ part1-linear-regression/part1_writeup.md
@@ -6,6 +6,12 @@ After completing `a6_part1.py` answer the following questions
 
 1. What is the r squared value?  What does this say about this linear regression model?
 
+The r^2 value is a value that takes the distance between each point on the graph and the line of best fit, sprinkles it with math fairy dust, and spits out a value between -1 and 1. This tells you the correalation between the two models given that the line of best fit is in the optimal format for the graph(y=mx=b for linear relationships, y=a(x-h)^2+k for exponential, etc).
+
 2. According to your model, what is the predicted systolic blood pressure for a person who is 42 years old?
 
+According to the model, 137mmHg.
+
 3. How accurate do you think this model's predictions are?  Do you think this model is accurate enough to reliably predict someone's blood pressure based on their age?  Why or why not?  And if not, what could improve the model?
+
+I have a suspiscion that it could be improved by considering more variables, since the human body is normally more complex than plug in your age and get a value. However I understand that this is a coding class and is therefore not at all related to medical practice in any way, so for our use, yes, this data is acceptable.
